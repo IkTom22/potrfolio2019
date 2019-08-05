53208a4dd355145114a76e53e750da4741e9c54f
Iko <ikotominaga@gmail.com>
2019-08-06 08:08:21 +1200

    basic html, package.json, npm added

---
+++ index.js
@@ -0,0 +1,12 @@
+// const hypno = document.querySelector('.hypno');
+// hypno.addEventListener('click', () => {
+//     if (hypno.classList.contains('turn')) {
+//         console.log('need to remove the class!');
+//         hypno.classList.toggle('turn');
+//     } else {
+//         hypno.classList.add('turn');
+//         hypno.classList.remove('turn');
+//     }
+
+// })
+
