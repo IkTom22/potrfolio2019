605dcf872747976b9d09f4d196cfc0f82d15d1ca
Iko <ikotominaga@gmail.com>
2019-08-22 13:28:30 +1200

    landing images added

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// const hypno = document.querySelector('.hypno');
-// hypno.addEventListener('click', () => {
-//     if (hypno.classList.contains('turn')) {
-//         console.log('need to remove the class!');
-//         hypno.classList.toggle('turn');
-//     } else {
-//         hypno.classList.add('turn');
-//         hypno.classList.remove('turn');
-//     }
 
-// })
+let hypno = document.querySelector('.hypno');
+hypno.addEventListener('click', () => {
+    if (hypno.classList.contains('turn')) {
+        hypno.classList.remove('turn');
+    }
+    hypno.classList.add('turn');
+    setTimeout(() => { hypno.classList.remove('turn'); }, 2000);
+
+})
 
